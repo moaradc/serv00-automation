044ca1fd0321ad6a3c69b6d4340b409cb90abb00
moaradc <143486936+moaradc@users.noreply.github.com>
2024-07-19 23:47:34 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -35,16 +35,20 @@ time = datetime.now(beijing_timezone).strftime('%Y-%m-%d %H:%M:%S')
 
 loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
 
-sct_sendkey = os.getenv('SCT_SENDKEY')
+pushplus_token = os.getenv('PUSHPLUS_TOKEN')
 
 title = 'serv00 服务器登录提醒'
-content = f"用户：{', '.join(user_list)}, 登录了 SSH 服务器\n登录时间：{time}\n登录IP：{loginip}"
-url = f'https://sctapi.ftqq.com/{SCT_SENDKEY}.send'
+content = f"用户：{', '.join(user_list)}, 登录了 SSH 服务器<br>登录时间：{time}<br>登录IP：{loginip}"
+url = 'http://www.pushplus.plus/send'
 data = {
+    "token": pushplus_token,
     "title": title,
-    "desp": content
+    "content": content
 }
-response = requests.post(url, data=data)
+body = json.dumps(data).encode(encoding='utf-8')
+headers = {'Content-Type': 'application/json'}
+
+response = requests.post(url, data=body, headers=headers)
 if response.status_code == 200:
     print("推送成功")
 else:
